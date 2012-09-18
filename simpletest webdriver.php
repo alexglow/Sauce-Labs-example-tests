a1d617adf9296e73174dff08b98c56cfb68ebf34
Alex Glowaski <alex@saucelabs.com>
2012-09-17 17:27:11 -0700

    Adding Simpletest (PHP) + WebDriver + Sauce

---
+++ simpletest webdriver.php
@@ -0,0 +1,24 @@
+// This is an example of using Simpletest (http://simpletest.org/en/start-testing.html) with Selenium 2 (WebDriver) on Sauce Labs' cross-browser testing VMs.
+
+<?php
+require_once('simpletest/autorun.php');
+
+class TestSeleniumOnSauce extends UnitTestCase {
+    
+    function setUp() {
+        $wd_host = 'http://<USERNAME>:<PASSWORD>@ondemand.saucelabs.com:80/wd/hub';
+        $web_driver = new PHPWebDriver_WebDriver($wd_host);
+        $this->session = $web_driver->session('firefox');
+    }
+    
+    function tearDown() {
+        $this->session->close();
+    }
+    
+    function testLoggingInIsLogged() {
+        $this->session->open('http://saucelabs.com/test/guinea-pig');
+        $this->assertEqual($this->session->title(),
+                           "I am a page title - Sauce Labs");
+    }
+}
+?>
