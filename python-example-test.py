64d0d8dfad66e7a3e75f36b4e45ad11816bf02a2
Alex Glowaski <alex@saucelabs.com>
2013-05-14 13:36:27 +0100

    expanding, annotating, & formatting

--- python-example-test.py
+++ python-example-test.py
@@ -2,7 +2,6 @@
 
 import unittest
 from selenium import webdriver
-from time import sleep
 
 class GLaDOS(unittest.TestCase):
 
@@ -17,6 +16,10 @@ class GLaDOS(unittest.TestCase):
         desired_capabilities['custom-data'] = { "release": "1.0", "commit": "0k392a9dkjr", "staging": "true" }
         desired_capabilities['idle-timeout'] = 60 #seconds
         
+        # for mobile devices; "deviceType" is for Android only
+        #desired_capabilities['deviceOrientation'] = 'landscape'
+        #desired_capabilities['deviceType'] = "tablet"
+        
         self.driver = webdriver.Remote(
             desired_capabilities=desired_capabilities,
             command_executor="http://USERNAME:API-KEY@ondemand.saucelabs.com:80/wd/hub")
@@ -29,30 +32,36 @@ class GLaDOS(unittest.TestCase):
         self.driver.find_element_by_link_text('i am a link').click()
         self.driver.back()
 
+        # Interact with inputs
         commentbox = self.driver.find_element_by_id('comments')
         commentbox.send_keys('hello, world!')
-        commenttext = "\n" + commentbox.get_attribute('value') + "\n"
-        print commenttext
-        
         commentbox.clear()
         commentbox.send_keys('hello again!')
         commentbox.submit()
+        
+        # Check for content
         body = self.driver.find_element_by_xpath('//body')
         self.assertTrue('hello again!' in body.text)
-        
-        sleep(20) # Use sparingly! Implicit waits are usually better.
         
         html_source = self.driver.page_source
         self.assertTrue('i_am_an_id' in html_source)
         
+        thetextbox = self.driver.find_element_by_id('i_am_a_textbox')
+        testtext = "\n" + thetextbox.get_attribute('value') + "\n"
+        print testtext
+                
         # Write page source to a file in PWD
         ascii_source = html_source.encode('ascii', 'ignore')
         source_file = open("pagesource.txt", "w")
         source_file.write(ascii_source)
         source_file.close()
         
-        self.driver.get_screenshot_as_file('guineapig.png') # Also saves to PWD
-        
+        # Scroll and screenshot
+        self.driver.get('https://twitter.com/Horse_Recruiter')
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") # blog.varunin.com/2011/08/scrolling-on-pages-using-selenium.html
+        self.driver.execute_script("window.scrollBy(0,200);")
+        self.driver.get_screenshot_as_file('horse_recruiter.png') # Also saves to PWD
+                
         sesh = self.driver.session_id
         print "Link to your job: https://saucelabs.com/jobs/%s" % sesh
         
